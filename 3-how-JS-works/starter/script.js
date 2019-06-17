9c5f4b6b1171201cad459093f931b854b75e21c1
Evgeni-Borovik <anyuser1980@gmail.com>
2019-06-17 20:59:13 +0300

    2nd commit hoisting of func declarations and vars

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,6 +1,7 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+//functions
 calculateAge(1965);
 
 function calculateAge (year) {
@@ -8,17 +9,24 @@ function calculateAge (year) {
 }
 
 
+//retirement(1990); -> ERROR "undefined"!
+var retirement = function (year) {
+    console.log(65 - (2016 - year));
+}
 
 
 
+//variables
 
+var age = 23;
+console.log(age);
 
-
-
-
-
-
-
+function foo() {
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 
 
 ///////////////////////////////////////
