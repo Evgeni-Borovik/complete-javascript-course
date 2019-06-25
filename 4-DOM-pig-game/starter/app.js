e8baca2dd00dbea7c962b16786911b8ddc0fead7
Evgeni-Borovik <anyuser1980@gmail.com>
2019-06-25 23:39:37 +0300

    fix: fix init function

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -21,6 +21,13 @@ function init() {
     document.getElementById("score-1").textContent = "0";
     document.getElementById("current-0").textContent = "0";
     document.getElementById("current-1").textContent = "0";
+    document.getElementById("name-0").textContent = "Player 1";
+    document.getElementById("name-1").textContent = "Player 2";
+    document.querySelector(".player-0-panel").classList.remove("winner");
+    document.querySelector(".player-1-panel").classList.remove("winner");
+    document.querySelector(".player-0-panel").classList.remove("active");
+    document.querySelector(".player-1-panel").classList.remove("active");
+    document.querySelector(".player-0-panel").classList.add("active");
 };
 
 function nextPlayer() {
@@ -68,6 +75,4 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
 });
 
 
-document.querySelector(".btn-new").addEventListener("click", function () {
-    init();
-});
+document.querySelector(".btn-new").addEventListener("click", init);
